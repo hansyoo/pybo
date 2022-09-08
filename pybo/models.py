bf0eddac4724213c8311a93e406727d89f2bfdc7
유한성 <hansyoo@m-road.co.kr>
2022-09-08 11:48:40 +0900

    파이보 프로젝트 최초 커밋

---
+++ pybo/models.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Question(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="author_question")
+    subject = models.CharField(max_length=200)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
+    voter = models.ManyToManyField(User, related_name="voter_question")  # 추천인 추가
+
+    def __str__(self):
+        return self.subject
+
+
+class Answer(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="author_answer")
+    # 기존 모델(Question)을 속성으로 연결하기 위해서 ForeignKey를 사용한다.
+    # 연결된 Question이 삭제되면 Answer도 함께 삭제되도록 on_delete..
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
+    voter = models.ManyToManyField(User, related_name='voter_answer')
